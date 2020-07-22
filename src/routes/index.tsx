e4650aafc01e1bf8654101024b56d81e8e75e269
Anderson Menezes <andss@MacBook-Pro-de-Anderson.local>
2020-07-21 23:53:14 -0300

    Initialized Github Explorer

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+import { deflate } from 'zlib';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
+);
+
+export default Routes;
